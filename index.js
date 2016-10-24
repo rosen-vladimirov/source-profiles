708de0dffcaec75be70a8c86abc2b0478f75d86f
rosen-vladimirov <rosen.vladimirov@telerik.com>
2016-10-25 00:30:04 +0300

    Improve functionality and add unit tests

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
- // TODO: any of the dotted files might not exist
-// List all dotted files from the home dir and source them maybe?
-// http://hayne.net/MacDev/Notes/unixFAQ.html#shellStartup
-// Correct order for Login shells is:
 "use strict";
 
 const fs = require("fs"),
 	childProcess = require("child_process"),
-	path = require("path");
+	path = require("path"),
+	_ = require("lodash");
 
+// Correct order for Login shells is described here:
+// http://hayne.net/MacDev/Notes/unixFAQ.html#shellStartup
 const profileOrder = [
 	".bash_profile",
 	".bash_login",
@@ -15,8 +14,9 @@ const profileOrder = [
 	".bashrc"
 ];
 
-let getPathToProfile = (shell) => {
-	let currentProfileOrder = profileOrder.map(p => path.join("~", p.replace("bash", shell)));
+const getPathToProfile = (shell) => {
+	let currentProfileOrder = profileOrder.map(p => p.replace("bash", shell)) //in case the default shell is not bash
+		.map(p => path.join(process.env.HOME, p));
 
 	for(let profileName of currentProfileOrder) {
 		if (fs.existsSync(profileName)) {
@@ -25,27 +25,28 @@ let getPathToProfile = (shell) => {
 	}
 };
 
-let getEnironmentVariables = () => {
-	let profileName = getPathToProfile(process.env.SHELL || "bash");
+const getEnvironmentVariables = () => {
+	let profileName = getPathToProfile(process.env && process.env.SHELL && path.basename(process.env.SHELL) || "bash");
 
-	let bashEnv = childProcess.execSync(`source ${profileName} && env`).toString();
+	let bashEnv = childProcess.execSync(`${process.env.SHELL || "/bin/bash"} -c "source ${profileName} && env"`).toString();
 
 	let bashVars = {};
 
-	bashEnv.split('\n').forEach(r => {
+	bashEnv.split('\n').forEach(row => {
 		// Environment variables cannot have = in their names, so we are safe with non-greedy regex.
 		// Do not trim at the end as variable values may have space(s) at the end.
-		let match = r.trimStart().match(/^(.*?)=(.*?)$/);
+		let match = _.trimStart(row).match(/^(.+?)=(.*?$)/);
+
 		if (match) {
-			bashVars[match[1]] = match[2];
+			bashVars[match[1].trim()] = match[2];
 		} else {
-			console.log(r + " does not match regex");
+			console.log(row + " does not match regex");
 		}
 	});
 
 	return bashVars;
-}
+};
 
 module.exports = {
-	getEnironmentVariables: getEnironmentVariables
+	getEnvironmentVariables: getEnvironmentVariables
 }
--- test/index.js
+++ test/index.js
@@ -1,5 +1,131 @@
-describe("index.js", () => {
-	it("inital test", () => {
-		require("../index");
+const _ = require("lodash"),
+	assert = require("chai").assert;
+
+describe("getEnvironmentVariables", () => {
+	it("returns correct variables", () => {
+		let fs = require("fs");
+		// Mock just to prevent multiple file system calls.
+		fs.existsSync = (filePath) => true;
+
+		const expectedVariables = {
+			"VAR1": "1",
+			"VAR2": "2",
+			"VAR4": "1=2=3=4",
+			"VAR5": "   1 2   3 ",
+			"VAR6": "1 2 3"
+		};
+
+		let childProcess = require("child_process");
+		childProcess.execSync = (command) => {
+			return _.map(expectedVariables, (value, key) => {
+				return `${key}=${value}`;
+			}).join('\n');
+		};
+
+		const actualResult = require("../index").getEnvironmentVariables();
+		assert.deepEqual(actualResult, expectedVariables);
+	});
+
+	it("prints warning when environment variable does not match expected format", () => {
+		let fs = require("fs");
+		// Mock just to prevent multiple file system calls.
+		fs.existsSync = (filePath) => true;
+
+		const expectedVariables = {
+			"": "1"
+		};
+
+		let childProcess = require("child_process");
+		childProcess.execSync = (command) => {
+			return _.map(expectedVariables, (value, key) => {
+				return `${key}=${value}`;
+			}).join('\n');
+		};
+
+		let loggedWarnings = [];
+		const originalConsoleLog = console.log;
+		console.log = (data) => {
+			loggedWarnings.push(data);
+		};
+
+		const actualResult = require("../index").getEnvironmentVariables();
+		console.log = originalConsoleLog;
+		assert.deepEqual(loggedWarnings.length, 1);
+		assert.isTrue(loggedWarnings[0].indexOf("does not match") !== -1);
+	});
+
+	describe("uses correct order of profiles", () => {
+		const profileOrder = [
+			".bash_profile",
+			".bash_login",
+			".profile",
+			".bashrc"
+		];
+
+		_.each(profileOrder, profileName => {
+			it (`when ${profileName} exists it's always used`, () => {
+				let fs = require("fs");
+				fs.existsSync = (filePath) => {
+					return _.endsWith(filePath, profileName);
+				};
+
+				const expectedVariables = {
+					"VAR1": "1"
+				};
+
+				let passedCommandArgument;
+
+				let childProcess = require("child_process");
+
+				childProcess.execSync = (command) => {
+					passedCommandArgument = command;
+					return _.map(expectedVariables, (value, key) => {
+						return `${key}=${value}`;
+					}).join('\n');
+				};
+
+				const actualResult = require("../index").getEnvironmentVariables();
+
+				assert.deepEqual(actualResult, expectedVariables);
+
+				assert.isTrue(passedCommandArgument.indexOf(profileName) !== -1);
+			});
+
+		});
+	});
+
+	it ("when default shell is not set, uses bash", () => {
+		let shell = process.env.SHELL;
+		process.env.SHELL = '';
+
+		const profileName = ".bash_profile";
+		let fs = require("fs");
+		fs.existsSync = (filePath) => {
+			return _.endsWith(filePath, profileName);
+		};
+
+		const expectedVariables = {
+			"VAR1": "1"
+		};
+
+		let passedCommandArgument;
+
+		let childProcess = require("child_process");
+
+		childProcess.execSync = (command) => {
+			passedCommandArgument = command;
+
+			return _.map(expectedVariables, (value, key) => {
+				return `${key}=${value}`;
+			}).join('\n');
+		};
+
+		const actualResult = require("../index").getEnvironmentVariables();
+
+		process.env.SHELL = shell;
+
+		assert.deepEqual(actualResult, expectedVariables);
+
+		assert.isTrue(passedCommandArgument.indexOf(profileName) !== -1);
 	});
 });
